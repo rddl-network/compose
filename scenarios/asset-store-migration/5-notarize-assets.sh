85cf32088edba5a658d42f56ed1a958e78cb9d2d
Lorenz Herzberger <lorenzherzberger@gmail.com>
2024-06-05 14:15:03 +0200

    notarize multiple assets
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- scenarios/asset-store-migration/5-notarize-assets.sh
+++ scenarios/asset-store-migration/5-notarize-assets.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-planetmint-god tx asset notarize-asset cid --from plmnt10mq5nj8jhh27z7ejnz2ql3nh0qhzjnfvy50877 --fees 1plmnt -y --keyring-backend test
+n=500
+
+# Loop to run the command n times
+for (( i=1; i<=n; i++ ))
+do  
+    # Generate a random string using openssl
+    random_value=$(openssl rand -base64 10 | tr -d /=+ | cut -c 1-10)
+
+    # Run the command with the random string
+    planetmint-god tx asset notarize-asset "$random_value" --from plmnt10mq5nj8jhh27z7ejnz2ql3nh0qhzjnfvy50877 --fees 1plmnt -y --keyring-backend test --offline -s $i -a 10
+done
